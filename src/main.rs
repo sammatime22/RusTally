86caf371f4c274a9d91a511f069637ac7fb25e64
sammyreles22 <samuel.reles@gmail.com>
2021-05-07 22:50:31 -0400

    Some progress on everything

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,81 @@
-use ferris_says::say;
-use std::io::{stdout, stdin, BufWriter};
+// Imports
+use std::io::{stdout, stdin, Write};
+use std::process::{exit};
 
-const WELCOME_TO_RUSTALLY:&str = "Welcome to RusTally!";
-const WELCOME_TO_RUSTALLY_SIZE:usize = 20;
+// Meta Commands
+const EXIT:&str = "EXIT\n";
 
+// Exit Constants
+const NO_ERROR_EXIT:i32 = 0;
+
+// Input Positions
+const META_COMMAND_POS:usize = 0;
 const KEY_POS:usize = 0;
 const VALUE_POS:usize = 1;
+
+/**
+ * Always prints the provided string to the output by always flushing the output buffer.
+ */
+fn definite_print(string_to_print: &str, println: bool) {
+    if println {
+        println!("{}", string_to_print);
+    } else {
+        print!("{}", string_to_print);
+    }
+
+    stdout().flush().expect("flush failed");
+}
+
+/**
+ * This method will interpret the input of the command and properly adjust totals of tally.
+ */
+fn interpret_input() {
+
+}
 
 /**
  * Gathers input from the CLI and places the resultant values into the string storage memory 
  * pointed two by the two pointers provided.
  */
-fn gather_input(mut key: &str, mut value: &str) {
-    println!(">: ");
+fn gather_input(key: &mut String, value: &mut String) -> bool {
+    definite_print(">: ", false);
+    
     let mut input = String::new();
-    stdin().read_line(&mut input).expect("Malformed entry.");
+    stdin().read_line(&mut input).unwrap();
 
     let split_input = input.split(|c| (c == ' ')).collect::<Vec<&str>>();
 
-    if split_input.len() == 2 {
-        (key) = split_input[KEY_POS];
-        (value) = split_input[VALUE_POS];
+    // Potentially a valid meta command
+    if split_input.len() == 1 {
+        if EXIT.to_string().eq(split_input[META_COMMAND_POS]) {
+            exit(NO_ERROR_EXIT);
+        } else {
+            println!("Thing {} {}", split_input[META_COMMAND_POS], EXIT.to_string());
+            stdout().flush().expect("flush failed");
+        }
     }
+
+    // Actually gather values from the CLI
+    if split_input.len() == 2 {
+        *key = split_input[KEY_POS].to_string();
+        *value = split_input[VALUE_POS].to_string();
+        return true;
+    }
+
+    return false;
 }
 
 /**
  * The main method.
  */
 fn main() {
-    let stdout = stdout();
-    let mut writer = BufWriter::new(stdout.lock());
-    say(WELCOME_TO_RUSTALLY.as_bytes(), WELCOME_TO_RUSTALLY_SIZE, &mut writer).unwrap();
-
     let mut key = String::new();
     let mut value = String::new();
 
     // Main Loop, will have to turn this into a separate class in the near future.
     loop {
-        gather_input(&mut key, &mut value);
-        println!("Provided Key {} : Value {}", key, value)
+        if gather_input(&mut key, &mut value) {
+            println!("Provided Key {} : Value {}", key, value);
+        }
     }
 }
