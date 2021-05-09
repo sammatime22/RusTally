3f76e9068b31a696031c965daffcfdbbc0dfc097
sammyreles22 <samuel.reles@gmail.com>
2021-05-09 16:54:30 -0400

    Got in memory tallys working

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,8 @@ use std::io::{stdout, stdin, Write};
 use std::process::{exit};
 
 // Meta Commands
-const EXIT:&str = "EXIT\n";
-const SAVE:&str = "SAVE\n";
+const EXIT:&str = "EXIT";
+const SAVE:&str = "SAVE";
 
 // Exit Constants
 const NO_ERROR_EXIT:i32 = 0;
@@ -14,13 +14,11 @@ const NO_ERROR_EXIT:i32 = 0;
 const META_COMMAND_POS:usize = 0;
 const KEY_POS:usize = 0;
 const VALUE_POS:usize = 1;
+const PARSED_VALUE_POS:usize = 1;
 
 // Math Constants
-const PLUS:&str = "+";
-const MINUS:&str = "-";
-
-// Our Table
-item table::HashMap = HashMap::new();
+const PLUS:char = '+';
+const MINUS:char = '-';
 
 /**
  * Always prints the provided string to the output by always flushing the output buffer.
@@ -38,25 +36,32 @@ fn definite_print(string_to_print: String, println: bool) {
 /**
  * Discerns the integer tally value provided. Any decimals will be ignored.
  */ 
-fn evaluate_value(&value) -> i32 {
-    if PLUS.to_string().eq(value.to_string().substring(0,1)) {
-        return value.split(|c| c == PLUS).parse::<i32>;
+fn evaluate_value(value: &str) -> i32 {
+    if PLUS.to_string().eq(&value[..1]) {
+        let cut_value = value.split(|c| (c == PLUS)).collect::<Vec<&str>>();
+        definite_print(cut_value[PARSED_VALUE_POS].to_string(), true);
+        return cut_value[PARSED_VALUE_POS].parse::<i32>().unwrap();
+    } else if MINUS.to_string().eq(&value[..1]) {
+        let cut_value = value.split(|c| (c == MINUS)).collect::<Vec<&str>>();
+        return -1 * cut_value[PARSED_VALUE_POS].parse::<i32>().unwrap();        
     } else {
-        return -1 * value.split(|c| c == MINUS).parse::<i32>;
+        return 0;
     }
 }
 
 /**
  * This method will interpret the input of the command and properly adjust totals of tally.
  */
-fn interpret_input(&key, &value) {
-    if table.get(key) == None {
-        table.insert(key, evaluate_value(value));
+fn interpret_input(key: &str, value: &str, table: &mut HashMap<String, i32>) {
+    if !(*table).contains_key(&key.to_string()) {
+        (*table).insert(key.to_string(), evaluate_value(value));
     } else {
-        if let Some(value) = map.get_mut(key) {
-            *value += evaluate_value(value);
+        if let Some(key_value) = (*table).get_mut(key) {
+            *key_value += evaluate_value(value);
         }
     }
+
+    definite_print(format!("New Value: {}: {:?}", key, (*table).get(key)), true);
 }
 
 /**
@@ -83,7 +88,7 @@ fn gather_input(key: &mut String, value: &mut String) -> bool {
     let mut input = String::new();
     stdin().read_line(&mut input).unwrap();
 
-    let split_input = input.split(|c| (c == ' ')).collect::<Vec<&str>>();
+    let split_input = input.trim_right().split(|c| (c == ' ')).collect::<Vec<&str>>();
 
     // Potentially a valid meta command
     if split_input.len() == 1 {
@@ -93,7 +98,7 @@ fn gather_input(key: &mut String, value: &mut String) -> bool {
             save_data();
         } else {
             definite_print(
-                format!("Thing {} {}", split_input[META_COMMAND_POS], EXIT.to_string()), true
+                format!("Meta Command {} Not Comprehendable", split_input[META_COMMAND_POS]), true
             );
         }
     }
@@ -114,6 +119,8 @@ fn gather_input(key: &mut String, value: &mut String) -> bool {
 fn main() {
     // If file provided, load it into the class definied struct
     
+    // Our Table
+    let mut table:HashMap<String, i32> = HashMap::new();
 
     let mut key = String::new();
     let mut value = String::new();
@@ -122,7 +129,7 @@ fn main() {
     loop {
         if gather_input(&mut key, &mut value) {
             definite_print(format!("Provided Key {} : Value {}", key, value), true);
-            interpret_input(&key, &value);
+            interpret_input(&key, &value, &mut table);
         }
     }
 }
